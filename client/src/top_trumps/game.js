355c641d665ca3d62cccb8b68f74f142fb10699f
Bertie Croll <bert.croll@gmail.com>
2016-12-15 13:40:22 +0000

    handSize parameter added to Game constructor

--- client/src/top_trumps/game.js
+++ client/src/top_trumps/game.js
@@ -1,11 +1,15 @@
-var Game = function(deck) {
+var Game = function(deck, handSize) {
   this.deck = deck;
+  this.handSize = handSize;
   this.players = [];
 };
 
 Game.prototype = {
   playerCount: function() {
     return this.players.length;
+  },
+  addPlayer: function(newPlayer) {
+    this.players.push(newPlayer);
   }
 };
 
--- client/src/top_trumps/specs/game_spec.js
+++ client/src/top_trumps/specs/game_spec.js
@@ -5,8 +5,8 @@ var game;
 
 describe("Game", function() {
   before(function() {
-    var stubDeck = ["Ryu", "Ken", "Vega"];
-    game = new Game(stubDeck);
+    var stubDeck = [1, 2, 3, 4, 5, 6];
+    game = new Game(stubDeck, 3);
   });
 
   it("should start with no players", function() {
@@ -16,4 +16,14 @@ describe("Game", function() {
   it("should start with a deck", function() {
     assert.equal(true, game.deck.length > 0);
   });
+
+  it("should be able add a player", function() {
+    var player1Stub = {name: "Diana", hand: []};
+    game.addPlayer(player1Stub);
+    assert.equal(1, game.playerCount());
+  });
+
+  it("should have a hand size", function() {
+    assert.equal(3, game.handSize);
+  })
 });
