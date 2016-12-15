5ef2b14bf7cef1f80095d089db973a9080c43a99
Paul Milne <hallhill@gmail.com>
2016-12-15 12:04:42 +0000

    refining first deck test :

--- client/src/top_trumps/specs/deck_spec.js
+++ client/src/top_trumps/specs/deck_spec.js
@@ -2,8 +2,8 @@ var Deck = require("../deck.js");
 var assert = require("assert");
 
 describe("Deck", function(){
-     it("should be an array of objects", function(){
-          var deck = new Deck([{name: "Fighter1", strength: 15}, {name: "Fighter2", strength: 12}]);
-          assert("2", deck.length);
+     it("start as an empty array", function(){
+          var deck = new Deck();
+          assert(0, deck.length);
      })
 })
