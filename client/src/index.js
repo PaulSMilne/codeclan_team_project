cd6b058489dbbbb9857a0e277885d05a009d496c
Bertie Croll <bert.croll@gmail.com>
2016-12-16 20:38:12 +0000

    country_flag images added

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ var GameView = require('./views/game_view.js');
 
 var app = function() {
   getFighters();
+  getVenues();
 };
 
 var getFighters = function() {
@@ -17,6 +18,24 @@ var getFighters = function() {
     var jsonString = event.target.responseText;
     var data = JSON.parse(jsonString);
     createNewGame(data);
+  };
+  request.send();
+};
+
+var getVenues = function() {
+  var url = "http://localhost:3000/venues";
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+
+  request.onload = function(event) {
+    if (event.target.status !== 200) return;
+    var directory = "images/venues/";
+    var jsonString = event.target.responseText;
+    var data = JSON.parse(jsonString);
+    var img = document.createElement('img');
+    img.src = directory + data.countries[0].venues[0].image;
+    var splash = document.getElementById('splash');
+    splash.appendChild(img);
   };
   request.send();
 };
