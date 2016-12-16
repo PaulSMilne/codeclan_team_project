307b0ca33989190f889d5d60b20c45043107f799
Bertie Croll <bert.croll@gmail.com>
2016-12-16 11:29:48 +0000

    display method added to game_view

--- client/src/views/game_view.js
+++ client/src/views/game_view.js
@@ -2,7 +2,42 @@ var Player = require('../top_trumps/player.js');
 
 var GameView = function(game) {
   this.game = game;
-  console.log(game);
 };
+
+GameView.prototype = {
+  display: function() {
+    var gameSection = document.getElementById('game');
+    var currentCard = {
+      id: 1,
+      name: "Abel",
+      homeCountry: "France",
+      fightingStyle: "MMA",
+      quote: "You've got good skills, but you have some growing to do before going pro.",
+      image: "http://vignette2.wikia.nocookie.net/streetfighter/images/4/49/SFVAbel.jpg/revision/latest?cb=20160629143935",
+      abilities: {
+                  strength: 14,
+                  agility:  10,
+                  defense:  11,
+                  intelligence: 7,
+                  charm: 5,
+                  resolve: 16,
+                  range: 12
+                },
+      specialMoves: ["Grappling", "Wheel Kick", "Marseilles Roll", "Falling Sky", "Tornado"]
+    };
+    var fighterCard = document.createElement('div');
+    var image = document.createElement('img');
+    image.src = currentCard.image;
+    var abilitiesList = document.createElement('ul');
+    for(ability in currentCard.abilities) {
+      var listItem = document.createElement('li');
+      listItem.innerText = ability + ": " + currentCard.abilities[ability];
+      abilitiesList.appendChild(listItem);
+    }
+    fighterCard.appendChild(image);
+    fighterCard.appendChild(abilitiesList);
+    gameSection.appendChild(fighterCard);
+  }
+}
 
 module.exports = GameView;
