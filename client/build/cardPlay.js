9cd5dc91d72ba74a2438df512f3156aa5e08ed92
Paul Milne <hallhill@gmail.com>
2016-12-18 01:37:13 +0000

    got a script to get name and image data from json data and put it into a card

--- client/build/cardPlay.js
+++ client/build/cardPlay.js
@@ -4,7 +4,7 @@ var fighter = [{
       "homeCountry": "France",
       "fightingStyle": "MMA",
       "quote": "You've got good skills, but you have some growing to do before going pro.",
-      "image": "http://vignette2.wikia.nocookie.net/streetfighter/images/4/49/SFVAbel.jpg/revision/latest?cb=20160629143935",
+      "image": "/images/characters/abel.png",
       "abilities": {
                   "strength": 14,
                   "agility":  10,
@@ -18,11 +18,26 @@ var fighter = [{
     }];
 // console.log(fighter[0].abilities);
 
-var cardPlay = function (jsonObject, index){
-     var powers = jsonObject[index].abilities;
+// var cardPlay = function (jsonObject, index){
+//      var powers = jsonObject[index].abilities;
+//      var imageURL = jsonObject[index].image;
+//      var player1Card = document.querySelector("#player1Card");
+//      console.log(player1Card);
+     // player1Card.setAttribute("style", "background:no-repeat center 5px url('/images/characters/chun-li.png')");
+
      // return powers;
-     console.log(powers.strength);
+     // console.log(powers.strength);
+// }
+window.onload = function (){
+     var powers = fighter[0].abilities;
+     var imageURL = fighter[0].image;
+     var player2Card = document.querySelector("#player2Card");
+     var player2head = document.querySelector("#player2Card h1");
+     player2head.innerText = fighter[0].name;
+     var player1buttons = document.querySelectorAll("#player1Card li button")
+     var player2buttons = document.querySelectorAll("#player2Card li")
+     console.log(player1buttons);
+     player2Card.setAttribute("style", "background:no-repeat center 5px url('" + fighter[0].image + "')");
 }
 
 
-window.onload = cardPlay(fighter,0);
