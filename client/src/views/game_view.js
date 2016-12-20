a75e04af646e80b742ad37c9d891db0cd2aacf9a
Paul Milne <hallhill@gmail.com>
2016-12-20 06:52:39 +0000

    added icons under the players name to represent card count

--- client/src/views/game_view.js
+++ client/src/views/game_view.js
@@ -44,10 +44,10 @@ GameView.prototype = {
     var multiplier = this.game.players[0].cardCount();
     var cardsCount = ("&nbsp;<img src='/images/sf-icon.jpg'>").repeat(multiplier);
     var p1name = document.createElement('h2');
-    p1name.innerHTML = this.game.players[0].name + cardsCount;
+    p1name.innerText = this.game.players[0].name;
     player1bar.appendChild(p1name);
     var p1cardCount = document.createElement('h3');
-    p1cardCount.innerText = this.game.players[0].cardCount() + " cards in hand";
+    p1cardCount.innerHTML = cardsCount;
     player1bar.appendChild(p1cardCount); 
 
     player2bar = document.getElementById('player2Bar');
@@ -55,10 +55,10 @@ GameView.prototype = {
     var multiplier2 = this.game.players[1].cardCount();
     var cardsCount2 = ("&nbsp;<img src='/images/sf-icon.jpg'>").repeat(multiplier2);
     var p2name = document.createElement('h2');
-    p2name.innerHTML = this.game.players[1].name + cardsCount2;
+    p2name.innerText = this.game.players[1].name;
     player2bar.appendChild(p2name);
     var p2cardCount = document.createElement('h3');
-    p2cardCount.innerText = this.game.players[1].cardCount() + " cards in hand";
+    p2cardCount.innerHTML = cardsCount2;
     player2bar.appendChild(p2cardCount); 
 
   },
